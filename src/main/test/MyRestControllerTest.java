4550fdcb1636f32bc9586612d2d54703c588388c
User <al_rasim98@mail.ru>
2024-06-02 21:39:56 +0500

    Test jUnit+Mockito

---
+++ src/main/test/MyRestControllerTest.java
@@ -0,0 +1,54 @@
+import dancebase.springmvc.security.rest.controller.MyRestController;
+import dancebase.springmvc.security.rest.entities.Dancer;
+import dancebase.springmvc.security.rest.service.DancerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class MyRestControllerTest {
+    @Mock
+    private DancerService dancerService;
+
+    @InjectMocks
+    private MyRestController restController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(){
+        mockMvc= MockMvcBuilders.standaloneSetup(restController).build();
+    }
+
+    @Test
+    public void getAllDancers() throws Exception {
+        List<Dancer> dancers=new ArrayList<>();
+        Dancer dancer=new Dancer("Rasim","Adad",1998,"asdasdad");
+        dancers.add(dancer);
+        when(dancerService.getAllDancers()).thenReturn(dancers);
+        mockMvc.perform(get("/dancers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Rasim"))
+                .andExpect(jsonPath("$[0].surname").value("Adad"))
+                .andExpect(jsonPath("$[0].year").value(1998))
+                .andExpect(jsonPath("$[0].danceClub").value("asdasdad"));
+    }
+
+
+}
